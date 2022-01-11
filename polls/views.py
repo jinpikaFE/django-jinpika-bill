54407c2a103942596c1db0e4892a8435f3e855c1
jinxin_fl <972784646@qq.com>
2022-01-11 16:37:01 +0800

    feat: 接口初编

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+import json
+
+from django.core import serializers
+from django.forms import model_to_dict
+from django.http import HttpResponse, JsonResponse
+from rest_framework import serializers
+
+from polls.models import Question
 
 
 def index(request):
@@ -6,4 +13,13 @@ def index(request):
 
 
 def test(request):
-    return HttpResponse('test')
+    if request.method == "GET":
+        dic = {}
+        result = serializers.serialize("json",Question.objects.all())
+        # L = JsonResponse(list(result), safe=False)
+        # result = serializers.serialize("json", result)
+        # print(serializers.serialize("json", result))
+        dic['code'] = '1'
+        dic['message'] = ''
+        dic['result'] = result
+        return HttpResponse(result)
